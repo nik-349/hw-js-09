fbbf088361201dcdbd57c3661bb42c22f6b94440
nik-349 <nik349@icloud.com>
2022-03-30 18:16:17 +0300

    parc-src

---
+++ src/js/03-promises.js
@@ -0,0 +1,13 @@
+// import Notiflix from 'notiflix';
+
+
+
+
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  if (shouldResolve) {
+    // Fulfill
+  } else {
+    // Reject
+  }
+}
